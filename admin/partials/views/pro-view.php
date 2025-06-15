91230280296966679ed195dc460266388d42d077
Danuka Prasad <121777864+danuabd@users.noreply.github.com>
2025-06-15 23:20:55 +0530

    Add HTML for pro-view

---
+++ admin/partials/views/pro-view.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * The view for the "Pro" tab in the admin settings page.
+ *
+ * @link       https://danukaprasad.com
+ * @since      1.0.0
+ *
+ * @package    Wp_Tawk_To_Integrator
+ * @subpackage Wp_Tawk_To_Integrator/admin/partials/views
+ */
+if (! defined('WPINC')) {
+    die;
+}
+?>
+<div
+    data-relation="pro"
+    id="pro-section"
+    class="hidden tab-content pro-section-container my-6 space-y-6">
+    <div class="pro-section-overlay-text">
+        Pro Features Coming Soon!
+    </div>
+    <div class="blurred-section">
+        <h5 class="text-xl font-semibold mb-2">
+            Get access to all features by unlocking Pro version!
+        </h5>
+        <p class="text-gray-600">
+            Explore various ways to use your Tawk.to chat widget for your
+            business, portfolio, e-commerce store!
+        </p>
+        <ul class="list-disc list-inside mt-4 text-gray-500">
+            <li>Advanced Webhook Integrations (Zapier, etc.)</li>
+            <li>Deeper WooCommerce &amp; EDD Integration</li>
+            <li>Customizable Chat Widget Styling Presets</li>
+            <li>Priority Support</li>
+            <li>And much more!</li>
+        </ul>
+    </div>
+</div>
