8a865648300bff0b9eea19e5361c25cfbda7f713
Danuka Prasad <121777864+danuabd@users.noreply.github.com>
2025-06-15 22:35:30 +0530

    Remove unnecessary comments from activator.php and modify some

--- includes/class-wp-tawk-to-integrator-activator.php
+++ includes/class-wp-tawk-to-integrator-activator.php
@@ -8,29 +8,17 @@
  *
  * @package    Wp_Tawk_To_Integrator
  * @subpackage Wp_Tawk_To_Integrator/includes
- */
-
-/**
- * Fired during plugin activation.
- *
- * This class defines all code necessary to run during the plugin's activation.
- *
- * @since      1.0.0
- * @package    Wp_Tawk_To_Integrator
- * @subpackage Wp_Tawk_To_Integrator/includes
  * @author     ABD Prasad <contact@danukaprasad.com>
  */
-class Wp_Tawk_To_Integrator_Activator {
+class Wp_Tawk_To_Integrator_Activator
+{
 
 	/**
-	 * Short Description. (use period)
+	 * Start from clean slate.
 	 *
-	 * Long Description.
+	 * Remove any previous plugin data if exists.
 	 *
 	 * @since    1.0.0
 	 */
-	public static function activate() {
-
-	}
-
+	public static function activate() {}
 }
