8f305ac4a72df805d7fb429e2e8ca1e7eee7aba1
Danuka Prasad <121777864+danuabd@users.noreply.github.com>
2025-06-17 00:09:25 +0530

    Fix CSS styling issues

--- includes/class-wp-tawk-to-integrator.php
+++ includes/class-wp-tawk-to-integrator.php
@@ -163,7 +163,7 @@ class Wp_Tawk_To_Integrator
 
 		$plugin_public = new Wp_Tawk_To_Integrator_Public($this->get_plugin_name(), $this->get_version());
 
-		$this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_styles');
+		$this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_styles', 99999);
 		$this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_scripts');
 
 		$this->loader->add_action('wp_footer', $plugin_public, 'embed_tawk_to_widget');
